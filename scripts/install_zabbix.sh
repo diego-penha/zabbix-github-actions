dde9ae49874781aabbf357f47bf43ce49febf601
Diego <diegoluiz_penha@hotmail.com>
2026-07-03 02:35:52 +0000

    Adiciona instalacao do zabbix

---
+++ scripts/install_zabbix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Atualizando pacotes..."
+sudo apt update
+
+echo "Instalando Zabbix Agent..."
+sudo apt install -y zabbix-agent2
+
+echo "Verificando serviço..."
+sudo systemctl enable zabbix-agent2
+sudo systemctl restart zabbix-agent2
+
+echo "Status:"
+systemctl is-active zabbix-agent2
+`
